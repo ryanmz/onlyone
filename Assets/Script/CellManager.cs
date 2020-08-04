bafc8163debabe33d01f6724c1d8362de72f88dd
J00599144 <31576041+J00599144@users.noreply.github.com>
2020-08-05 03:05:16 +0800

    Demo
    核心框架Demo

---
+++ Assets/Script/CellManager.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CellManager : MonoBehaviour
+{
+    public RectTransform tagCell;
+    public float distance;
+    public float speed;
+    public RectTransform player;
+
+
+    private Vector2 dir;
+    private DragHandler[,] grids = new DragHandler[2, 2];
+    // Start is called before the first frame update
+    void Start()
+    {
+        DragHandler[] cells = GetComponentsInChildren<DragHandler>();
+        int index = 0;
+        for(int i = 0; i < grids.GetLength(0); i++)
+        {
+            for(int j = 0; j < grids.GetLength(1); j++)
+            {
+                grids[i, j] = cells[index];
+                Debug.Log(grids[i, j].gameObject.name);
+                index++;
+            }
+            
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.dir = (this.tagCell.position - this.player.position).normalized;
+        this.player.Translate(this.dir * Time.deltaTime * this.speed);
+
+        this.distance = Vector2.Distance(this.tagCell.position, this.player.position);
+
+        if (this.distance < 10.0f)
+        {
+            this.tagCell = this.SelectNewCell(this.tagCell.GetComponent<DragHandler>().currentDir);
+
+        }
+
+    }
+
+    public RectTransform SelectNewCell(DirectionEnum dir)
+    {
+        if(dir == DirectionEnum.cUpDir)
+        {
+            if (this.GetRow() < 1)
+            {
+                return this.tagCell;
+            }
+            else
+            {
+                return this.grids[this.GetRow() - 1, this.GetCol()].gameObject.GetComponent<RectTransform>();
+            }
+        }
+        else if (dir == DirectionEnum.cDownDir)
+        {
+            if (this.GetRow() == (this.grids.GetLength(0)-1))
+            {
+                return this.tagCell;
+            }
+            else
+            {
+                return this.grids[this.GetRow() + 1, this.GetCol()].gameObject.GetComponent<RectTransform>();
+            }
+        }
+        else if (dir == DirectionEnum.cLeftDir)
+        {
+            if (this.GetCol() < 1)
+            {
+                return this.tagCell;
+            }
+            else
+            {
+                return this.grids[this.GetRow(), this.GetCol() - 1].gameObject.GetComponent<RectTransform>();
+            }
+        }
+        else if (dir == DirectionEnum.cRightDir)
+        {
+            if (this.GetCol() == (this.grids.GetLength(1) - 1))
+            {
+                return this.tagCell;
+            }
+            else
+            {
+                return this.grids[this.GetRow(), this.GetCol() + 1].gameObject.GetComponent<RectTransform>();
+            }
+        }
+        return this.tagCell;
+    }
+    private int GetRow()
+    {
+
+        for (int i = 0; i < this.grids.GetLength(0); i++)
+        {
+            for (int j = 0; j < this.grids.GetLength(1); j++)
+            {
+                if(this.tagCell.GetComponent<DragHandler>() == this.grids[i, j])
+                {
+                    return i;
+                }
+            }
+
+        }
+        return 0;
+    }
+    private int GetCol()
+    {
+
+        for (int i = 0; i < this.grids.GetLength(0); i++)
+        {
+            for (int j = 0; j < this.grids.GetLength(1); j++)
+            {
+                if (this.tagCell.GetComponent<DragHandler>() == this.grids[i, j])
+                {
+                    return j;
+                }
+            }
+
+        }
+        return 0;
+    }
+
+}
---
+++ Assets/Script/DragHandler.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragHandler : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    //公共变量
+    #region
+    public DirectionEnum currentDir;
+    public bool enableClick = false;
+    #endregion
+
+    //私有变量
+    #region
+    private RectTransform rectTran;
+    private Image colorImg;
+    private Color originCol;
+    #endregion
+
+    //周期函数
+    #region
+    void Start()
+    {
+        this.rectTran = this.GetComponent<RectTransform>();
+        this.originCol = this.GetComponent<Image>().color;
+
+
+        currentDir = CommonFunction.Instance.JudgeDir(rectTran.transform.localEulerAngles.z);
+
+    }
+
+    #endregion
+
+    //接口函数
+    #region
+    //当鼠标按下时调用 接口对应 IPointerDownHandler
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (this.enableClick)
+        {
+            this.colorImg = this.GetComponent<Image>();
+            this.colorImg.color = Color.green;
+        }
+
+    }
+
+    //当鼠标拖动时调用 对应接口 IDragHandler
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (this.enableClick)
+        {
+            RectTransform canvas = GameObject.Find("Canvas").GetComponent<RectTransform>();
+            Vector2 mouseDown = eventData.position;
+            Vector2 mouseUguiPos = new Vector2();
+
+            bool isRect = RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas, mouseDown, eventData.enterEventCamera, out mouseUguiPos);
+
+            if (isRect)
+            {
+                Vector2 dir = (mouseUguiPos - this.rectTran.anchoredPosition).normalized;
+                float faceToValue = 0.0f;
+
+                if (Math.Abs(dir.y) > Math.Abs(dir.x) && dir.y > 0)
+                {
+
+                    faceToValue = CommonFunction.Instance.DirectValue(DirectionEnum.cUpDir);
+                }
+                else if (Math.Abs(dir.y) > Math.Abs(dir.x) && dir.y < 0)
+                {
+
+                    faceToValue = CommonFunction.Instance.DirectValue(DirectionEnum.cDownDir);
+                }
+                else if (Math.Abs(dir.y) < Math.Abs(dir.x) && dir.x > 0)
+                {
+
+                    faceToValue = CommonFunction.Instance.DirectValue(DirectionEnum.cRightDir);
+                }
+                else if (Math.Abs(dir.y) < Math.Abs(dir.x) && dir.x < 0)
+                {
+
+                    faceToValue = CommonFunction.Instance.DirectValue(DirectionEnum.cLeftDir);
+                }
+                this.currentDir = CommonFunction.Instance.JudgeDir(faceToValue);
+                this.rectTran.localRotation = Quaternion.Euler(0, 0, faceToValue);
+            }
+        }
+
+    }
+
+    //当鼠标抬起时调用 对应接口 IPointerUpHandler
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (this.enableClick)
+        {
+            this.colorImg = this.GetComponent<Image>();
+            this.colorImg.color = originCol;
+        }
+    }
+    #endregion
+
+
+
+
+}
