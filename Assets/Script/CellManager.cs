f8b1472492f939f22a2ae307560b1fa454e540a5
ryanmz <ryan_qmz@qq.com>
2020-08-05 23:56:08 +0800

    Update CellManager.cs
    跟踪维护了角色的移动方向，并新增了各个格子的功能

--- Assets/Script/CellManager.cs
+++ Assets/Script/CellManager.cs
@@ -7,11 +7,15 @@ using UnityEngine.UI;
 public class CellManager : MonoBehaviour
 {
     public RectTransform tagCell;
+    public RectTransform startCell;      // 起点
     public float distance;
     public float speed;
     public RectTransform player;
 
-    public DirectionEnum characterDir;
+    public DirectionEnum characterDir;   // 角色的移动方向
+    public bool clear = false;           // 通关标识
+    public bool over = false;            // 游戏结束标识
+    public int actionPoint = 1;          // 行动点
 
     private Vector2 dir;
     private DragHandler[,] grids = new DragHandler[2, 2];
@@ -44,24 +48,77 @@ public class CellManager : MonoBehaviour
 
         if (this.distance < 10.0f)
         {
-            this.tagCell = this.ArrowSelectNewCell(this.tagCell.GetComponent<DragHandler>().currentDir);
+            this.tagCell = this.SelectNewCell(this.tagCell.GetComponent<DragHandler>().currentDir);
 
         }
 
     }
 
-    // 空白格功能
+    // 起点(输入为起始方向)
     #region
-    public RectTransform BlankSelectNewCell()
+    public RectTransform StartCell(DirectionEnum dir)
     {
+        return this.SelectNewCell(dir);
+    }
+    #endregion
 
+    // 终点
+    #region
+    public RectTransform EndCell()
+    {
+        clear = true;
         return this.tagCell;
     }
     #endregion
 
-    // 箭头格功能 
+    // 空白格
     #region
-    public RectTransform ArrowSelectNewCell(DirectionEnum dir)
+    public RectTransform BlankCell()
+    {
+        return this.SelectNewCell(this.characterDir);
+    }
+    #endregion
+
+    // 指向格
+    #region
+    public RectTransform ArrowCell(DirectionEnum dir)
+    {
+        return this.SelectNewCell(dir);
+    }
+    #endregion
+
+    // 行动点+1格
+    #region
+    public RectTransform ActionPointCell()
+    {
+        actionPoint++;
+        return this.SelectNewCell(this.characterDir);
+    }
+    #endregion
+
+    // 问号格
+    #region
+    public RectTransform UnknownCell()
+    {
+        DirectionEnum dir = 0;
+        // ... 修改格子属性并得到要移动的方向 dir
+        return this.SelectNewCell(dir);
+    }
+    #endregion
+
+    // 伤害格
+    #region
+    public RectTransform DamageCell()
+    {
+        over = true;
+        // ... 初始化全局
+        return this.startCell;  // 返回起点位置
+    }
+    #endregion
+
+    // 移动 
+    #region
+    public RectTransform SelectNewCell(DirectionEnum dir)
     {
         if(dir == DirectionEnum.cUpDir)
         {
@@ -71,6 +128,7 @@ public class CellManager : MonoBehaviour
             }
             else
             {
+                this.characterDir = DirectionEnum.cUpDir;
                 return this.grids[this.GetRow() - 1, this.GetCol()].gameObject.GetComponent<RectTransform>();
             }
         }
@@ -82,6 +140,7 @@ public class CellManager : MonoBehaviour
             }
             else
             {
+                this.characterDir = DirectionEnum.cDownDir;
                 return this.grids[this.GetRow() + 1, this.GetCol()].gameObject.GetComponent<RectTransform>();
             }
         }
@@ -93,6 +152,7 @@ public class CellManager : MonoBehaviour
             }
             else
             {
+                this.characterDir = DirectionEnum.cLeftDir;
                 return this.grids[this.GetRow(), this.GetCol() - 1].gameObject.GetComponent<RectTransform>();
             }
         }
@@ -104,6 +164,7 @@ public class CellManager : MonoBehaviour
             }
             else
             {
+                this.characterDir = DirectionEnum.cRightDir;
                 return this.grids[this.GetRow(), this.GetCol() + 1].gameObject.GetComponent<RectTransform>();
             }
         }
